eb3480d0679e61d237009fa8fa4dc09ba882b3bc
AleksKar <Alekseykar92@gmail.com>
2023-04-18 20:11:45 +0300

    feat/s-1: refact, add func addTask: main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 // находим элементы на странице
 const form = document.querySelector("#form");
 const taskInput = document.querySelector("#taskInput");
-const tasksList = document.querySelector('#tasksList');
+const tasksList = document.querySelector("#tasksList");
 
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", addTask);
+
+function addTask(event) {
   // отмена отправки формы
   event.preventDefault();
 
@@ -12,27 +14,27 @@ form.addEventListener("submit", function (event) {
 
   // формируем размеку для новой задачи
   const taskHTML = `
-  <li class="list-group-item d-flex justify-content-between task-item">
-    <span class="task-title">${taskText}</span>
-    <div class="task-item__buttons">
-      <button type="button" data-action="done" class="btn-action">
-        <img src="./img/tick.svg" alt="Done" width="18" height="18">
-      </button>
-      <button type="button" data-action="delete" class="btn-action">
-      <img src="./img/cross.svg" alt="Done" width="18" height="18">
-      </button>
-    </div>
-  </li>`;
+    <li class="list-group-item d-flex justify-content-between task-item">
+      <span class="task-title">${taskText}</span>
+      <div class="task-item__buttons">
+        <button type="button" data-action="done" class="btn-action">
+          <img src="./img/tick.svg" alt="Done" width="18" height="18">
+        </button>
+        <button type="button" data-action="delete" class="btn-action">
+        <img src="./img/cross.svg" alt="Done" width="18" height="18">
+        </button>
+      </div>
+    </li>`;
 
-  // Добавляем задачу на страницу 
-  tasksList.insertAdjacentHTML('beforeend', taskHTML);
+  // Добавляем задачу на страницу
+  tasksList.insertAdjacentHTML("beforeend", taskHTML);
 
   // Очищаем поле ввода и сохраняем на него фокус
-  taskInput.value = '';
+  taskInput.value = "";
   taskInput.focus();
 
   // Проверка, если список пополняется элементами, то строка "Список" скрывается
-  if(tasksList.children.length > 1) {
-    emptyList.classList.add('none')
+  if (tasksList.children.length > 1) {
+    emptyList.classList.add("none");
   }
-});
+}
